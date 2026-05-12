e1fec6bddf1f2448ea63c211875b33ac29af3f07
Ruwais1 <liruwais1@gmail.com>
2026-05-12 15:11:07 +0000

    Task 2: Create executor.c to run commands using fork and execve

---
+++ executor.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * execute_command - Executes a command using fork and execve
+ * @args: Array of arguments (parsed user input)
+ *
+ * Return: Always 1 (to continue the shell loop)
+ */
+int execute_command(char **args)
+{
+	pid_t pid;
+	int status;
+
+	if (args[0] == NULL)
+		return (1);
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror(args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+
+	return (1);
+}
