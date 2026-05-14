13c27ed8d06d6c12275a32f16fbaaf5630b14547
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-05-14 06:15:51 +0300

    Refactor main function for clarity and structure

--- main.c
+++ main.c
@@ -1,40 +1,39 @@
 #include "shell.h"
+
 /**
- * main - Entry point for the simple shell
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
-	char *line = NULL;
+	char *line = NULL, **args;
 	size_t len = 0;
 	ssize_t read_bytes;
-	char **args;
+
 	while (1)
-	{ if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "($) ", 4);
-	 read_bytes = getline(&line, &len, stdin);
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "($) ", 4);
+		read_bytes = getline(&line, &len, stdin);
 		if (read_bytes == -1)
-		{
 			break;
-		}
 		args = split_line(line);
-		if (args != NULL && args[0] != NULL)
-		{ if (strcmp(args[0], "exit") == 0)
-			{  
+		if (args && args[0])
+		{
+			if (strcmp(args[0], "exit") == 0)
+			{
 				free(args);
-			}   handle_exit(line);
-		  if (strcmp(args[0], "env") == 0)
-			{ 
-				print_env();
-				free(args);
-				continue;
+				handle_exit(line);
 			}
-			  
-			execute_command(args);
+			if (strcmp(args[0], "env") == 0)
+				print_env();
+			else
+				execute_command(args);
 			free(args);
-		} else if (args != NULL)
-		{ free(args);
 		}
-return (0);
+		else if (args)
+			free(args);
 	}
+	free(line);
+	return (0);
 }
