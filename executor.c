08a07ba65ffb3165eee480fc193e91f53da6d1aa
Ruwais1 <liruwais1@gmail.com>
2026-05-15 01:47:56 +0000

    Update executor

--- executor.c
+++ executor.c
@@ -4,34 +4,44 @@
  * execute_command - Executes a command using fork and execve
  * @args: Array of arguments (parsed user input)
  *
- * Return: Always 1 (to continue the shell loop)
+ * Return: Exit status of the executed command
  */
 int execute_command(char **args)
 {
 	pid_t pid;
 	int status;
+	char *cmd_path;
 
 	if (args[0] == NULL)
 		return (1);
-
+	cmd_path = get_command_path(args[0]);
+	if (cmd_path == NULL)
+	{
+		perror(args[0]);
+		return (127);
+	}
 	pid = fork();
 	if (pid == -1)
 	{
 		perror("Error");
+		free(cmd_path);
 		return (1);
 	}
 	if (pid == 0)
 	{
-		if (execve(args[0], args, environ) == -1)
+		if (execve(cmd_path, args, environ) == -1)
 		{
 			perror(args[0]);
+			free(cmd_path);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
 		wait(&status);
+		free(cmd_path);
 	}
-
-	return (WEXITSTATUS(status));
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (1);
 }
